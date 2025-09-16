4a69712d00481e4d9495f0b54e74e2aa5b7586af
Owen Wang <owenwang2007@icloud.com>
2025-09-16 12:43:10 -0400

    Add a semi functional species graph

--- app/species-speed/animal-speed-graph.tsx
+++ app/species-speed/animal-speed-graph.tsx
@@ -1,13 +1,17 @@
 /* eslint-disable */
 "use client";
-import { select } from "d3-selection";
+import * as d3 from "d3";
 import { useEffect, useRef, useState } from "react";
 
 // Example data: Only the first three rows are provided as an example
 // Add more animals or change up the style as you desire
 
 // TODO: Write this interface
-interface AnimalDatum {}
+interface AnimalDatum {
+  name: string;
+  diet: string;
+  speed: number;
+}
 
 export default function AnimalSpeedGraph() {
   // useRef creates a reference to the div where D3 will draw the chart.
@@ -15,11 +19,25 @@ export default function AnimalSpeedGraph() {
   const graphRef = useRef<HTMLDivElement>(null);
 
   const [animalData, setAnimalData] = useState<AnimalDatum[]>([]);
+  const [counter, setCounter] = useState(0);
 
   // TODO: Load CSV data
   useEffect(() => {
-    console.log("Implement CSV loading!");
+    d3.csv("/sample_animals.csv").then((rawData) => {
+      // Transform the raw data to fit the AnimalDatum interface
+      const formattedData = rawData.map((animal) => ({
+        name: animal.Animal,
+        diet: animal.Diet,
+        speed: parseFloat(animal["Average Speed (km/h)"]),
+      }));
+      setAnimalData(formattedData);
+    });
   }, []);
+
+  function handleClick() {
+    setCounter(counter + 1);
+    console.log(animalData);
+  }
 
   useEffect(() => {
     // Clear any previous SVG to avoid duplicates when React hot-reloads
@@ -40,7 +58,7 @@ export default function AnimalSpeedGraph() {
 
     // Create the SVG element where D3 will draw the chart
     // https://github.com/d3/d3-selection
-    const svg = select(graphRef.current!).append<SVGSVGElement>("svg").attr("width", width).attr("height", height);
+    const svg = d3.select(graphRef.current!).append<SVGSVGElement>("svg").attr("width", width + 50).attr("height", height + 100);
 
     // TODO: Implement the rest of the graph
     // HINT: Look up the documentation at these links
@@ -48,13 +66,68 @@ export default function AnimalSpeedGraph() {
     // https://github.com/d3/d3-scale#linear-scales
     // https://github.com/d3/d3-scale#ordinal-scales
     // https://github.com/d3/d3-axis
+
+    var x = d3
+      .scaleBand()
+      .range([0, width])
+      .domain(
+        animalData.map(function (d) {
+          return d.name;
+        }),
+      )
+      .padding(0.2);
+
+    svg
+      .append("g")
+      .attr("transform", "translate(0," + height + ")")
+      .call(d3.axisBottom(x))
+      .selectAll("text")
+      .attr("transform", "translate(-10,0)rotate(-45)")
+      .style("text-anchor", "end");
+
+    // Add Y axis
+    var y = d3.scaleLinear().domain([0, 120]).range([height, 0]);
+    svg.append("g").call(d3.axisLeft(y));
+
+    // Bars
+    svg
+      .selectAll("mybar")
+      .data(animalData)
+      .enter()
+      .append("rect")
+      .attr("x", function (d) {
+        return x(d.name);
+      })
+      .attr("y", function (d) {
+        return y(d.speed);
+      })
+      .attr("width", x.bandwidth())
+      .attr("height", function (d) {
+        return height - y(d.speed);
+      })
+      .attr("fill", function (d) {
+        if (d.diet === "Carnivore") return "red";
+        if (d.diet === "Herbivore") return "green";
+        if (d.diet === "Omnivore") return "brown";
+        return "#a51c30"; // Default color if diet is unknown
+      });
+
+    svg
+      .append("text")
+      .attr("transform", "translate(" + width / 2 + " ," + (height + margin.bottom) + ")")
+      .style("text-anchor", "middle")
+      .text("Animal Names");
+
+    // Add Y-axis label
+    svg
+      .append("text")
+      .attr("transform", "rotate(-90)")
+      .attr("y", 0)
+      .attr("x", -(height / 2))
+      .style("text-anchor", "middle")
+      .text("Average Speed (km/h)");
   }, [animalData]);
 
   // TODO: Return the graph
-  return (
-    // Placeholder so that this compiles. Delete this below:
-    <div>
-      <h1> TODO: Delete this div in `animal-speed-graph.tsx` and implement the graph: </h1>
-    </div>
-  );
+  return <div ref={graphRef}></div>;
 }
